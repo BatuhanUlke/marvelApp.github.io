05e044ce726dba680234147a0b02fdd28802b4a8
BatuhanUlke <95655340+BatuhanUlke@users.noreply.github.com>
2024-02-05 14:32:14 +0300

    add new component

--- src/components/Characters.js
+++ src/components/Characters.js
@@ -1,13 +1,14 @@
 import React, { useState, useEffect } from 'react';
 
-import axios from 'axios';
+
 
 import CharacterTables from './CharacterTables';
 import Search from './Search';
+import { getCharacter } from '../services/CharacterService';
 
 
-// const hash='65dea527c78bcc3f5c6d00c378ea71dd'
-const hash='65dea527c78bcc3f5c6d00c378ea71dd'
+
+
 
 function Characters() {
   const [items, setItems] = useState([])
@@ -18,17 +19,9 @@ function Characters() {
  
   useEffect(()=>{
     const fetch=async()=>{
-      if(query===''){
-        const result=await axios.get(`https://gateway.marvel.com/v1/public/characters?&ts=1&apikey=e6d1499355872cf43ec216e18ee71830&hash=${hash}&limit=30`)
-        // console.log(result.data.data.results);
-        setItems(result.data.data.results);
-        
-      }else{
-        const result=await axios.get(`https://gateway.marvel.com/v1/public/characters?nameStartsWith=${query}&ts=1&apikey=72cc9b14584490fa249bc0efb7d5625e&hash=${hash}`)
-      //  console.log(result.data.data.results);
-       setItems(result.data.data.results);
+      const response= await getCharacter(query);
       
-      }
+      setItems(response.data.data.results)
        
     }
     fetch();
---
+++ src/services/CharacterService.js
@@ -0,0 +1,5 @@
+import axios from 'axios'
+export const getCharacter=async(query)=>{
+    const hash='65dea527c78bcc3f5c6d00c378ea71dd'
+    return await axios.get(`https://gateway.marvel.com/v1/public/characters?${query?`nameStartsWith=${query}`:""}&ts=1&apikey=e6d1499355872cf43ec216e18ee71830&hash=${hash}&limit=30`)
+}
