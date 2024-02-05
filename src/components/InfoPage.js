ada5133af9cfd5ec7721a3a9b222198e947f37e6
BatuhanUlke <95655340+BatuhanUlke@users.noreply.github.com>
2024-02-05 03:11:46 +0300

    new update

--- src/components/InfoPage.js
+++ src/components/InfoPage.js
@@ -4,11 +4,14 @@ import axios from 'axios';
 function InfoPage() {
     const {id}=useParams();
     const [items, setItems] = useState()
+    
     const fetch=async()=>{
        const response=await axios(`http://gateway.marvel.com/v1/public/characters/${id}?ts=1&apikey=72cc9b14584490fa249bc0efb7d5625e&hash=a67c0e577b747b73ec70eb5c6cf6600a`)
        setItems(response.data.data.results[0])
+       
     }
     fetch();
+  
   return (
     <>
   {
@@ -22,16 +25,18 @@ function InfoPage() {
               <h1>{items.name}</h1>
                 <h3>{items.description}</h3>
                 <h4>Comics</h4>
-               <ul>
-               {
-                
-               items.comics.items.map((comic,index)=>
-               <li key={index}>
-              {comic.name}
-              </li>)
-         }
-
-               </ul>
+              
+              <ul>
+                    {
+                     
+                    items.comics.items.map((comic,index)=>
+                    <li key={index} >
+                   {comic.name}
+                   </li>)
+              }
+     
+                    </ul>
+                    
                <h4>Series</h4>
                <ul>
                 {
